c1ee1db47718a87f135f1c993f9eedc8b65afba3
Jan <jan.klecka12@gmail.com>
2023-11-01 16:53:38 +0100

    remove file

--- Shared/Rabbit_MQ/consumer.py
+++ Shared/Rabbit_MQ/consumer.py
@@ -6,10 +6,3 @@ class DemoConsumer:
         async with message.process():
             print("Message processing by simple worker")
             print(message.body.decode("utf-8"))
-
-
-# may spawn through asyncio and run multiple workes at the same time.
-async def worker(message):
-    async with message.iterator() as queue_iter:
-        async for message in queue_iter:
-            print("Processing" + message.body.decode("utf-8"))
